279b8b2275be5fb9f3a8d9352b8e718767c45a0c
difflow295-ops <difflow295@gmail.com>
2025-11-27 20:30:00 +0545

    moved funtions to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+
+pub use std::io;
+pub use std::env;
+pub use std::io::Write;
+pub use std::process;
+pub use std::fs;
+
+
+pub fn run_file(filename : &String) -> Result<(),io::Error> {
+    let file_content  = fs::read_to_string(filename)?;
+    run(&file_content);
+    Ok(())
+}
+
+
+pub fn run_prompt( ) {
+    let mut expression_buffer : String = String::new(); 
+    loop {
+        expression_buffer.clear();
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let n  = io::stdin().read_line(&mut expression_buffer).unwrap();
+        if n == 0 {
+            break;
+        }
+        let expression_buffer = expression_buffer.trim();
+        if expression_buffer == "quit" || expression_buffer == "exit" || expression_buffer.is_empty(){
+            break;
+        }  
+        run(&expression_buffer.to_string());
+    }
+}
+
+
+pub fn run (file_content_source : &String)  {
+    for words in file_content_source.split_whitespace()  {
+        print!("{words} ");
+    }
+    println!("");
+}
+
+pub fn err(line: i64, message: &str) {
+    report(line, "", message);
+}
+
+pub fn report(line: i64, where_: &str, message: &str) {
+    eprintln!("[line {line}] Error {where_}: {message}");
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,16 @@
-use std::io;
-use std::env;
-use std::io::Write;
-use std::process;
-use std::fs;
+use rlox::*;
 
-fn main ( ) -> Result<(),io::Error> {
-    let args  : Vec<String> = env::args().collect();
+fn main ( ) -> Result<(),rlox::io::Error> {
+    let args  : Vec<String> = rlox::env::args().collect();
     if args.len() < 1 {
         println!("Usages : rlox [script]");
         process::exit(64);
     } else if args.len() == 2 {
-        run_file(&args[0])?;
+        rlox::run_file(&args[1])?;
     } else {
-        run_prompt();
+        rlox::run_prompt();
     }
     Ok(())
 }
 
 
-fn run_file(filename : &String) -> Result<(),io::Error> {
-    let file_content  = fs::read_to_string(filename)?;
-    println!("{filename}");
-    println!("Read {file_content}");
-    run(&file_content);
-    todo!("Add functionality");
-}
-
-
-fn run_prompt( ) {
-    let mut expression_buffer : String = String::new(); 
-    loop {
-        expression_buffer.clear();
-        print!("> ");
-        io::stdout().flush().unwrap();
-        let n  = io::stdin().read_line(&mut expression_buffer).unwrap();
-        if n == 0 {
-            break;
-        }
-        if expression_buffer.trim() == "quit".to_string() || expression_buffer.trim() == "exit".to_string(){
-            break;
-        }  
-    }
-    todo!("Add functionality");
-}
-
-
-fn run (_file_content : &String)  {
-    todo!("Add functionality");
-}
